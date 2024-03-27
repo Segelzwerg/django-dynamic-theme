0dd439a03e42156064754fb9706a03a4b3c37ef3
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2024-03-27 12:46:22 +0100

    ADD: settings for tests

---
+++ tests/settings.py
@@ -0,0 +1,60 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = "django-dynamic-theme"
+
+ALLOWED_HOSTS = ["*"]
+
+# Application definition
+INSTALLED_APPS = ["tests", "django-dynamic-theme"]
+
+INSTALLED_APPS += [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.messages",
+    "django.contrib.sessions",
+]
+
+MIDDLEWARE = [
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+]
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ]
+        },
+    },
+]
+
+database_engine = os.environ.get("DATABASE_ENGINE", "sqlite")
+database_config = {
+    "sqlite": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    },
+}
+
+DATABASES = {
+    "default": database_config.get(database_engine),
+}
+
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "tests/media/")
+MEDIA_URL = "/media/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "tests/static/")
+STATIC_URL = "/static/"
