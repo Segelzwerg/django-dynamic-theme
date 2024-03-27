59209836706b785cb440ca33a240d4da3773064f
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2024-03-27 13:08:59 +0100

    FIX: app name

--- django_dynamic_theme/apps.py
+++ django_dynamic_theme/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ThemeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'theme'
+class DjangoDynamicThemeConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "django_dynamic_theme"
