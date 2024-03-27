f79531dc5bd1a7e53a44b9e9cfb5fcfc4804e79f
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2024-03-27 15:30:04 +0100

    UPDATE: test robustness for file paths

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,3 +1,5 @@
+from os import mkdir, path, remove, rmdir
+from shutil import rmtree
 from django_dynamic_theme.models import Background, Theme
 
 
@@ -5,10 +7,33 @@ from django.test import TestCase
 
 
 class ThemeModelTest(TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.theme_name = "test"
+        self.folder = "static/"
+        self.file_path = f"{self.folder}/{self.theme_name}.scss"
+        if not path.exists(self.folder):
+            mkdir(self.folder)
+        _ = open(self.file_path, mode="w+")
+
+    def tearDown(self) -> None:
+        super().tearDown()
+        if path.exists(self.file_path):
+            remove(self.file_path)
+
     def test_export(self):
         color: str = "F0F0F0"
         background = Background.objects.create(primary_bg=color)
-        theme = Theme.objects.create(name="theme", background=background)
+        theme = Theme.objects.create(name=self.theme_name, background=background)
+        expected_string = "body {background: F0F0F0;}"
+        self.assertEqual(expected_string, theme.export())
+
+    def test_export_without_pre_existing_file(self):
+        if path.exists(self.file_path):
+            remove(self.file_path)
+        color: str = "F0F0F0"
+        background = Background.objects.create(primary_bg=color)
+        theme = Theme.objects.create(name=self.theme_name, background=background)
         expected_string = "body {background: F0F0F0;}"
         self.assertEqual(expected_string, theme.export())
 
