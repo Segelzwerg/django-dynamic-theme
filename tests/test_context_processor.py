65b1793e49938bb4cb21745dfff23e2cf2f699c5
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2024-03-28 20:25:30 +0100

    ADD: admin default used in processor

--- tests/test_context_processor.py
+++ tests/test_context_processor.py
@@ -3,6 +3,7 @@
 from django.test import TestCase
 
 from django_dynamic_theme.context_processor import theme
+from django_dynamic_theme.models import Background, Theme
 
 
 class ThemeTest(TestCase):
@@ -10,3 +11,10 @@ class ThemeTest(TestCase):
 
     def test_theme_file(self):
         self.assertDictEqual({"theme_file": "theme.scss"}, theme(""))
+
+    def test_theme_file_defined_by_admin(self):
+        background = Background.objects.create(name="black", primary_bg="000000")
+        admin_theme = Theme.objects.create(
+            name="Admin", default=True, background=background
+        )
+        self.assertDictEqual({"theme_file": f"{admin_theme.name}.scss"}, theme(""))
