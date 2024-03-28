ce1b2a1d0ec0302a1aacf5092b456006c27e55af
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2024-03-28 20:25:44 +0100

    UPDATE: try to get the admin default first

--- django_dynamic_theme/context_processor.py
+++ django_dynamic_theme/context_processor.py
@@ -1,9 +1,16 @@
 """Defines extra contexts for this application globally."""
 
+from django_dynamic_theme.models import Theme
+
 
 def theme(request) -> dict[str, str]:
     """
     Defines the url to the theme to be used.
     Returns: url to theme in a dict.
     """
-    return {"theme_file": "theme.scss"}
+    # TODO: add user specific theme / admin defined default
+    try:
+        admin_default: Theme = Theme.objects.get(default=True)
+        return {"theme_file": f"{admin_default.name}.scss"}
+    except Theme.DoesNotExist:
+        return {"theme_file": "theme.scss"}
