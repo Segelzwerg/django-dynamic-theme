587eb8925d284050f83265e1b7012d409eac9e4e
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2024-03-30 20:39:16 +0100

    UPDATE: use read the docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,10 @@ release = "0.0.2"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["autoapi.extension"]
+extensions = [
+    "autoapi.extension",
+    "sphinx_rtd_theme",
+]
 
 autoapi_dirs = ["../../django_dynamic_theme"]
 
@@ -28,5 +31,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
