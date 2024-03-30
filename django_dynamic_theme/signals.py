86b105945b12ea46ec98495a5fda628529f64732
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2024-03-30 21:36:52 +0100

    ADD: signal to remove file

---
+++ django_dynamic_theme/signals.py
@@ -0,0 +1,19 @@
+"""Defines the signals for this app.
+"""
+
+from django.dispatch import receiver
+from django_dynamic_theme.models import Theme
+from django.db.models.signals import post_delete
+
+from django_dynamic_theme.utill.scss_editor import ScssEditor
+
+
+@receiver(post_delete, sender=Theme)
+def delete_file(sender, instance, *args, **kwargs) -> None:
+    """
+    Removes the scss file for a theme.
+    :param sender: not used
+    :param instance: the theme that has been remove.
+    """
+    scss_editor = ScssEditor(instance.path)
+    scss_editor.delete()
