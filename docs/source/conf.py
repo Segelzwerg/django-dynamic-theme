55f1384cc00dbdfa03234dd9b52bcbcf2532c00f
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2024-03-30 20:03:08 +0100

    UPDATAE: import of module

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,11 @@
 # Configuration file for the Sphinx documentation builder.
-#
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
+sys.path.append(os.path.abspath(os.path.join(__file__, "../../django_dynamic_theme")))
+
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
