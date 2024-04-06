e649f3928d5e60e0af26e6cf5cf4072b198e48b6
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2024-04-07 01:45:37 +0200

    ADD: hex to rgb converter

--- django_dynamic_theme/utill/color_converter.py
+++ django_dynamic_theme/utill/color_converter.py
@@ -1,9 +1,17 @@
 """Converts colors into different formats."""
 
 
-def hex_to_tuple(rgb: str) -> tuple[str, str, str]:
+def hex_to_tuple(hex: str) -> tuple[str, str, str]:
     """
     Splits a #XXYYZZ HEX string into a tuple of strs (XX,YY,ZZ)
     """
-    rgb = rgb.replace("#", "")
-    return tuple(map(str, (rgb[0:2], rgb[2:4], rgb[4:6])))
+    hex = hex.lstrip("#")
+    return tuple(map(str, (hex[0:2], hex[2:4], hex[4:6])))
+
+
+def hex_to_rgb_tuple(hex: str) -> tuple[int, int, int]:
+    """
+    Converts a hex string to a RGB tuple.
+    """
+    hex = hex.lstrip("#")
+    return tuple(int(hex[i:i + 2], 16) for i in (0, 2, 4))
