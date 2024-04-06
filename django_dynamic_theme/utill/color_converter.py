cfc0d3475d75b307d14a6d97cc273e3535ca9e0c
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2024-04-07 00:36:36 +0200

    UPDATE: ensure str

--- django_dynamic_theme/utill/color_converter.py
+++ django_dynamic_theme/utill/color_converter.py
@@ -6,4 +6,4 @@ def hex_to_tuple(rgb: str) -> tuple[str, str, str]:
     Splits a #XXYYZZ HEX string into a tuple of strs (XX,YY,ZZ)
     """
     rgb = rgb.replace("#", "")
-    return tuple((rgb[0:2], rgb[2:4], rgb[4:6]))
+    return tuple(map(str, (rgb[0:2], rgb[2:4], rgb[4:6])))
