9ab9608be3771c3d1ec68129f9153b999281e16a
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2024-04-30 22:57:17 +0200

    ADD: test for not default theme

--- tests/test_middleware.py
+++ tests/test_middleware.py
@@ -39,3 +39,21 @@ class MiddlewareTest(TestCase):
         self.assertEqual(2, get_response.call_count)
         self.assertEqual("200", response)
         self.assertTrue(path.exists(self.file_path))
+
+    def test_save_file_no_default(self):
+        color: str = "F0F0F0"
+        background: Background = Background.objects.create(primary_bg=color)
+        navbar: Navbar = Navbar.objects.create(
+            name="Test", background_color="#FFFF00", text_color="#111111"
+        )
+        Theme.objects.create(
+            name=self.theme_name, background=background, navbar=navbar, default=False
+        )
+        request = MagicMock()
+        get_response = MagicMock(side_effect=[UncompressableFileError, "200"])
+        middleware = CompressorHandleMiddleware(get_response)
+        response = middleware(request)
+        get_response.assert_called_with(request)
+        self.assertEqual(2, get_response.call_count)
+        self.assertEqual("200", response)
+        self.assertTrue(path.exists(self.file_path))
