1d12f285d80ba8fc46774871f4dd35354274df84
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2024-04-30 23:19:52 +0200

    UPDATE: raise custom error if theme is missing

--- django_dynamic_theme/middleware.py
+++ django_dynamic_theme/middleware.py
@@ -1,9 +1,18 @@
+"""Middlewares for the dynamic theme app."""
+
 from compressor.exceptions import UncompressableFileError
 
+from django_dynamic_theme.errors import ThemeMissingError
 from django_dynamic_theme.models import Theme
 
 
-class CompressorHandleMiddleware:
+class MissingThemeHandleMiddleware:
+    """
+    Detects if the theme file is missing.
+    If a default theme is set it will export this one.
+    If there is not default theme it will export the first one.
+    If no theme exists it will raise an error.
+    """
 
     def __init__(self, get_response):
         self.get_response = get_response
@@ -11,12 +20,13 @@ class CompressorHandleMiddleware:
     def __call__(self, request):
         try:
             response = self.get_response(request)
-        except UncompressableFileError:
+        except UncompressableFileError as uncomp_error:
             try:
                 theme = Theme.objects.get(default=True)
             except Theme.DoesNotExist:
                 theme = Theme.objects.first()
-            if theme is not None:
-                theme.write_export()
+            if theme is None:
+                raise ThemeMissingError from uncomp_error
+            theme.write_export()
             response = self.get_response(request)
         return response
