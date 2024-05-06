8a8c7442550a3d52d95e4b6a6a7e193056db58d7
Sean Cassidy <suomi.sean@gmail.com>
2024-05-06 21:53:32 +0100

    Added input validation for the colour hex code converter.

--- django_dynamic_theme/errors.py
+++ django_dynamic_theme/errors.py
@@ -5,3 +5,7 @@ class ThemeMissingError(Exception):
     """
     Is raised if not theme is set.
     """
+class NonHexValueError(Exception):
+    """
+    Is raised when a value that is not a valid HEX color string is passed to the color converter.
+    """
