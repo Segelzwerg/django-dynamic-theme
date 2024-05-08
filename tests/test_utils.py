35aa38cdffe223c6da9d7a0af44dbb60afcdc57f
Marcel <25705862+Segelzwerg@users.noreply.github.com>
2024-05-09 01:14:20 +0200

    Update tests/test_utils.py
    Signed-off-by: Marcel <25705862+Segelzwerg@users.noreply.github.com>

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -28,7 +28,7 @@ class ColorConverterTest(TestCase):
         hex = "#1234FF"
         expected_tuple = (18, 52, 255)
         self.assertEqual(expected_tuple, hex_to_rgb_tuple(hex))
-    
+
     def test_input_validation(self):
         hex = "#1234F"
         self.assertRaises(NonHexValueError, hex_to_rgb_tuple, hex)
