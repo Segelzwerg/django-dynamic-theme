257b52a8ae5aebb14797b19024f7d370161df216
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2024-08-21 21:52:24 +0200

    FIX: static root

--- example_app/example_app/settings.py
+++ example_app/example_app/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = "example/static"
 STATICFILES_FINDERS = [
     "compressor.finders.CompressorFinder",
 ]
