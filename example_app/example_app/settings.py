ed55fdd68e1e13ff65254915efbff51f065c6ab6
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2024-08-21 22:42:47 +0200

    FIX: typo

--- example_app/example_app/settings.py
+++ example_app/example_app/settings.py
@@ -65,7 +65,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "django_dynamic_theme-context_processor.theme",
+                "django_dynamic_theme.context_processor.theme",
             ],
         },
     },
